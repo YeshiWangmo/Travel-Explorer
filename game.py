3297553d54db2dd1e50a165d6b0d6269c458f7f8
YeshiWangmo <03230424.gcbs@rub.edu.bt>
2023-10-20 10:09:37 -0700

    setup of pygame logic

---
+++ game.py
@@ -0,0 +1,27 @@
+
+import pygame
+
+# Initialize the game
+pygame.init()
+
+# Set up the game window
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Travel Explore")
+#set the background color
+bg_color=("purple")
+screen.fill(bg_color)
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update game logic here
+
+    # Render graphics here
+
+    pygame.display.update()
+
+# Quit the game
+pygame.quit()
