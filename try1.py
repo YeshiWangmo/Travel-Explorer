6649be7d9de98b7dcf6df2892522140b3e10042c
YeshiWangmo <03230424.gcbs@rub.edu.bt>
2023-10-31 23:19:43 -0700

    added new code

--- try1.py
+++ try1.py
@@ -1,54 +1,72 @@
 import pygame
+import random
 
-# Initialize the game
+# Initialize Pygame
 pygame.init()
 
-# Set up the game window
-screen = pygame.display.set_mode((960, 680))
-pygame.display.set_caption("Travel Explore1")
+# Set up some constants
+WIDTH, HEIGHT = 800, 600 
+BACKGROUND_COLOR = (0, 0, 0)
+FOOD_COLOR = (255, 255, 255)
+player_COLOR = (255, 0, 0)
 
-# Set the background 
-background= pygame.image.load("cartoon-forest-background-pk4lm7s01x1t3ckt.jpg")
-screen.blit(background,(0,0))
-
-class player(pygame.sprite.Sprite):
-    def_int_(self, pos_x, pos_y)
-    super().init_()
-    self.sprite= []
-    self.sprite.append(pygame.image.load("still.png"))
-    self.sprite.append(pygame.image.load("left side.jpg"))
-    self.sprite.append(pygame.image.load("rifht side.jpg"))
+# Create the screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+# Create the player
+player = pygame.image.load("boy.png")
+player_rect = player.get_rect(topleft=(200,300))
 
+# Create the food
+food = pygame.Rect(random.randint(0, WIDTH - 20), 0, 20, 20)
 
 # Game loop
 running = True
-player_x = 100
-player_y = 500
-velocity = 5
-
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                player_x += velocity
-            elif event.key == pygame.K_LEFT:
-                player_x -= velocity
-            elif event.key == pygame.K_UP:
-                player_y -= velocity
-            elif event.key == pygame.K_DOWN:
-                player_y += velocity
+  # Fill the screen with the background color
+  screen.fill(BACKGROUND_COLOR)
 
-    player = pygame.image.load("still.png")
-    player = player.convert_alpha()
-    player_size = (100, 80)
-    player = pygame.transform.scale(player, player_size)
-    player_rect = player.get_rect(topleft=(player_x, player_y))
-    screen.blit(player, player_rect)
-    pygame.draw.rect(screen,(255,0,0),(150,650,200,30))
-    pygame.display.update
-# Quit the game
+  # Draw the player
+  screen.blit(player, player_rect)
+
+  # Draw the food
+  pygame.draw.rect(screen, FOOD_COLOR, food)
+
+  # Move the food down
+  food.y += 1
+
+  # If the food hits the bottom of the screen, reset its position
+  if food.y > HEIGHT:
+      food.x = random.randint(0, WIDTH - 20)
+      food.y = 0
+
+  # If the food hits the player, reset its position
+  if player_rect.colliderect(food):
+      food.x = random.randint(0, WIDTH - 20)
+      food.y = 0
+
+  # Handle events
+  for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          running = False
+      elif event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_LEFT:
+              player_rect.x -= 50
+          elif event.key == pygame.K_RIGHT:
+              player_rect.x += 50
+
+  # Update the display
+  pygame.display.flip()
+
+# Quit Pygame
 pygame.quit()
+
+
+
+
+
+
+  
+
+
