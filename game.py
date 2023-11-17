2305c6822daf07714070fa180b71c906ac1bf034
YeshiWangmo <03230424.gcbs@rub.edu.bt>
2023-11-16 19:17:11 -0800

    test

--- game.py
+++ game.py
@@ -1,89 +1,131 @@
 import pygame
 import random
 
-# Initialize Pygame
-pygame.init()
 # Set up some constants
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 800, 640
+VELOCITY = 80
 
-# Set up the game window
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Travel Explore")
 
-# Set the background
-background = pygame.image.load("forest.jpg")
-background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+def initialize_game():
+    pygame.init()
+    return pygame.display.set_mode((WIDTH, HEIGHT)), pygame.image.load("forest.jpg"), pygame.mixer.music.load("best-adventure-ever-122726.mp3")
 
-# Insert this line to load the background sound
-pygame.mixer.music.load("best-adventure-ever-122726.mp3")
 
-# Set the background sound to loop infinitely
-pygame.mixer.music.play(-1)
 
-# Create the player
-player = pygame.image.load("boy.png")
-player_right = pygame.image.load("boy.png")
-player_left = pygame.image.load("boy left.png") 
-player_direction = "left"
+def load_images():
+    return (
+        pygame.image.load("boy left.png"),
+        pygame.image.load("boy.png"),
+        pygame.transform.scale(pygame.image.load("bo.png"), (50, 80)),
+        pygame.transform.scale(pygame.image.load("Map_thumbnail.png"), (100, 120)),
+        pygame.transform.scale(pygame.image.load("tent_thumbnail.png"), (100, 90)),
+        pygame.transform.scale(pygame.image.load("boom.webp"), (50, 50)),
+        pygame.transform.scale(pygame.image.load("start button.jpg"), (800, 650)),
+    )
 
-player_rect = player.get_rect(topleft=(150, 400))
+def create_player():
+    return "left", player_left.get_rect(topleft=(150, 400))
 
-# Create the things to catch
-Map = pygame.image.load("Map_thumbnail.png")
-tent = pygame.image.load("tent_thumbnail.png")
-bottle = pygame.image.load("bo.png")
-boom = pygame.image.load("boom.webp")
-
-# Scale the images size
-bottle_resized = pygame.transform.scale(bottle, (50, 80))
-map_resized = pygame.transform.scale(Map, (100, 120))
-tent_resized = pygame.transform.scale(tent, (100, 90))
-boom_resized = pygame.transform.scale(boom, (50, 50))
-
-boom_speed = 1.5
-
-items = [
-    {"image": bottle_resized, "rect": bottle_resized.get_rect(topleft=(random.randint(0, WIDTH - 20), 0)), "speed": 1.3},
-    {"image": map_resized, "rect": map_resized.get_rect(topright=(random.randint(0, WIDTH - 10), 0)), "speed": 1.5},
-    {"image": tent_resized, "rect": tent_resized.get_rect(topleft=(random.randint(0, WIDTH - 20), 0)), "speed": 1},
-]
-
-boom_item = {
-    "image": boom_resized,
-    "rect": boom_resized.get_rect(topleft=(random.randint(0, WIDTH - 20), 0)),
-    "speed": 1
-}
-items.append(boom_item)
-start_button= pygame.image.load("start button.jpg")
-start_button_resized = pygame.transform.scale(start_button, (800, 650))
-
-# Game variables
-player_x = 150
-player_y = 350
-velocity = 80
-player_lives = 3  # Initial player lives
-score = 0  # Player's score
-start_game = False
+def create_items():
+    return [
+        {"image": bottle_resized, "rect": bottle_resized.get_rect(topleft=(random.randint(0, WIDTH - 20), 0)), "speed": 1.3},
+        {"image": map_resized, "rect": map_resized.get_rect(topright=(random.randint(0, WIDTH - 10), 0)), "speed": 1.5},
+        {"image": tent_resized, "rect": tent_resized.get_rect(topleft=(random.randint(0, WIDTH - 20), 0)), "speed": 1.0},
+        {"image": boom_resized, "rect": boom_resized.get_rect(topleft=(random.randint(0, WIDTH - 20), 0)), "speed": 1.0},
+    ]
 
 def reset_item_position(item):
     item["rect"].y = 0
     item["rect"].x = random.randint(0, WIDTH - item["rect"].width)
 
+def draw_start_screen(screen):
+    screen.blit(start_button_resized, (0, 0))
+    font = pygame.font.Font(None, 36)
+    text = font.render("Welcome to collect things for your travel experience", 1, ("black"))
+    text_1 = font.render(" Press SPACE to start", 1, ("magenta"))
+    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 3 - text.get_height() // 19))
+    screen.blit(text_1, (WIDTH // 3 - text.get_width() // 9, HEIGHT // 2 - text.get_height() // 15))
+    pygame.display.flip()
+
+def draw_game_over_screen(screen):
+    font = pygame.font.Font(None, 72)
+    text = font.render("Game Over", 1, (255, 0, 0))
+    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
+    pygame.display.flip()
+    pygame.time.delay(200)
+
+def game_loop(screen, background, player_direction, player_rect, player_x, player_y, items, player_lives, score):
+    pygame.mixer.music.play(-1)
+    running = True
+    while running:
+        screen.blit(background, (0, 0))
+
+        if player_direction == "left":
+            screen.blit(player_left, player_rect)
+        else:
+            screen.blit(player_right, player_rect)
+
+        player_rect.x = player_x
+        player_rect.y = player_y
+
+        for item in items:
+            screen.blit(item["image"], item["rect"])
+            item["rect"].y += item["speed"]
+
+            if item["rect"].y > HEIGHT:
+                reset_item_position(item)
+
+            if player_rect.colliderect(item["rect"]):
+                if item is not items[-1]:
+                    reset_item_position(item)
+                    score += 1
+                else:
+                    player_lives -= 1
+                    reset_item_position(item)
+
+        font = pygame.font.Font(None, 36)
+        text_lives = font.render("Lives: " + str(player_lives), 1, ("orange"))
+        text_score = font.render("Final Score: " + str(score), 1, ("purple"))
+        screen.blit(text_lives, (10, 10))
+        screen.blit(text_score, (WIDTH // 2 - text_score.get_width() // 2, HEIGHT // 2 + text_score.get_height()))
+
+        if player_lives <= 0:
+            draw_game_over_screen(screen)
+            running = False
+
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    player_x += VELOCITY
+                    player_direction = "right"
+                elif event.key == pygame.K_LEFT:
+                    player_x -= VELOCITY
+                    player_direction = "left"
+
+        if player_lives <= 0:
+            running = False
+
+    pygame.quit()
+
+# Main execution
+screen, background, music = initialize_game()
+player_left, player_right, bottle_resized, map_resized, tent_resized, boom_resized, start_button_resized = load_images()
+player_direction, player_rect = create_player()
+items = create_items()
+
+# Other variables
+player_x, player_y = 150, 400
+player_lives, score = 3, 0
+start_game = False
 
 # Start screen loop
 while not start_game:
-    # Draw the start screen
-    screen.blit(start_button_resized, (0, 0))
-    font = pygame.font.Font(None, 36)
-    text = font.render("Welcome to collect thing for your travel experience", 1, ("black"))
-    text_1 = font.render(" Press SPACE to start", 1, ("magenta"))
-    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 3 - text.get_height() // 19))
-    screen.blit(text_1, (WIDTH // 3- text.get_width() // 9, HEIGHT // 2- text.get_height() // 15))
+    draw_start_screen(screen)
 
-    # Update the display
-    pygame.display.flip()
-
-    # Handle events for the start screen
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             start_game = True
@@ -92,89 +134,4 @@ while not start_game:
                 start_game = True
 
 # Game loop
-running = True
-while running:
-    # Draw the background
-    screen.blit(background, (0, 0))
-
-    # Draw the player
-    if player_direction == "left":
-        screen.blit(player_left, player_rect)
-    else:
-        screen.blit(player_right, player_rect)
-
-    # Move the player
-    player_rect.x = player_x
-    player_rect.y = player_y
-
-    # Draw the items and move them down
-    for item in items:
-        # Draw the item
-        screen.blit(item["image"], item["rect"])
-
-        # Move the item down
-        item["rect"].y += item["speed"]
-
-        # If the item hits the bottom of the screen, reset its position
-        if item["rect"].y > HEIGHT:
-            reset_item_position(item)
-
-        # If the item hits the player, reset its position and update the score
-        if player_rect.colliderect(item["rect"]):
-            if item is not boom_item:
-                reset_item_position(item)
-                score += 1
-            else:
-                player_lives -= 1
-                reset_item_position(item)
-
-    # Display player lives
-    font = pygame.font.Font(None, 36)
-    text = font.render("Lives: " + str(player_lives), 1, ("orange"))
-    screen.blit(text, (10, 10))
-
-    
-    font = pygame.font.Font(None, 36)
-    text = font.render("Final Score: " + str(score), 1, ("purple"))
-    screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 + text.get_height()))
-
-    # Check for game over
-    if player_lives <= 0:
-        font = pygame.font.Font(None, 72)
-        text = font.render("Game Over", 1, (255, 0, 0))
-        screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
-        pygame.display.flip()
-        pygame.time.delay(200)
-        running= False
-
-    # Update the display
-    pygame.display.flip()
-
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                player_x += velocity
-                player_direction = "right"
-            elif event.key == pygame.K_LEFT:
-                player_x -= velocity
-                player_direction = "left"
-
-    # Quit Pygame if player lives are over
-    if player_lives <= 0:
-        running = False
-    
-
-# Quit Pygame
-pygame.quit()
-
-
-
-
-# reference
-# https://youtu.be/Ongc4EVqRjo?feature=shared
-# https://youtu.be/_jmGqrC-RQ8?feature=shared
-# https://youtu.be/7QX8y6R9Hi8?feature=shared 
-# ChatGPT, personal communication, October, 2023
+game_loop(screen, background, player_direction, player_rect, player_x, player_y, items, player_lives, score)
---
+++ pytest.py
@@ -0,0 +1,79 @@
+import unittest
+from unittest.mock import patch, MagicMock
+import pygame
+from game import initialize_game, load_images, create_player, create_items, reset_item_position, draw_start_screen, game_loop, WIDTH, HEIGHT,screen, draw_game_over_screen, background, player_direction,player_rect, player_x,player_y, items
+
+
+class TestGame(unittest.TestCase):
+   def test_initialize_game(self):
+        self.assertIsInstance(screen, pygame.Surface)
+
+   def setUp(self):
+        self.screen, _, _ = initialize_game()
+        self.images = load_images()
+        self.player_direction, self.player_rect = create_player()
+        self.items = create_items()
+        self.player_x, self.player_y = 150, 400
+        self.player_lives, self.score = 3, 0
+
+   @patch('pygame.display.set_mode')
+   @patch('pygame.font.Font')
+   @patch('pygame.display.flip')
+   def test_draw_start_screen(self, mock_flip, mock_font, mock_set_mode):
+       mock_screen = MagicMock()
+       mock_set_mode.return_value = mock_screen
+       mock_font_instance = MagicMock()
+       mock_font.return_value = mock_font_instance
+
+       draw_start_screen(mock_screen)
+
+       
+       self.assertIsNotNone(mock_screen.get_surface())
+
+       # Check if the correct images are being drawn on the screen
+       mock_screen.blit.assert_called_with(mock_font_instance.render.return_value, (WIDTH // 2 - mock_font_instance.render.return_value.get_width() // 2, HEIGHT // 3 - mock_font_instance.render.return_value.get_height() // 19))
+
+       # Check if the screen is being updated correctly
+       mock_flip.assert_called_once()
+
+   def test_load_images(self):
+       images = load_images()
+       self.assertIsInstance(images, tuple)
+       for image in images:
+           self.assertIsInstance(image, pygame.Surface)
+   
+   def test_create_player(self):
+        self.assertIsInstance(player_rect, pygame.Rect)
+        self.assertIn(player_direction, ["left", "right"])
+   
+   def test_create_items(self):
+        for item in items:
+            self.assertIsInstance(item["image"], pygame.Surface)
+            self.assertIsInstance(item["rect"], pygame.Rect)
+            self.assertIsInstance(item["speed"], float)
+
+   def test_reset_item_position(self):
+        item = {"rect": pygame.Rect(0, 0, 10, 10)}
+        reset_item_position(item)
+        self.assertEqual(item["rect"].y, 0)
+        self.assertTrue(0 <= item["rect"].x < WIDTH - item["rect"].width)
+
+   def test_game_loop(self):
+        # Mocking pygame events for testing purposes
+        pygame.event.post(pygame.event.Event(pygame.QUIT))
+        pygame.event.post(pygame.event.Event(pygame.KEYDOWN, {'key': pygame.K_RIGHT}))
+        pygame.event.post(pygame.event.Event(pygame.KEYDOWN, {'key': pygame.K_LEFT}))
+        pygame.event.post(pygame.event.Event(pygame.KEYDOWN, {'key': pygame.K_SPACE}))
+
+   def test_draw_game_over_screen(self):
+      pygame.init() # initialize Pygame
+      screen = pygame.display.set_mode((800, 600)) # create a screen
+      draw_game_over_screen(screen)
+
+   def setUp(self):
+       self.player_lives = 0
+
+ 
+
+if __name__ == '__main__':
+   unittest.main()
